8de6bf366f7206625d84efaf3864228f1c4685bc
Prisma4 <theprismarindark@yandex.com>
2026-02-21 14:31:31 +0400

    Small bugfix

--- backend/bot/utils.py
+++ backend/bot/utils.py
@@ -6,3 +6,17 @@ def split_message(text: str, max_length: int = 4096) -> List[str]:
         return [text]
 
     return [text[i:i + max_length] for i in range(0, len(text), max_length)]
+
+
+def parse_allowed_users_ids(id_str: str, delimiter: str = ",") -> List[int] | None:
+    str_list = [v.strip() for v in id_str.split(delimiter)]
+    id_list = []
+
+    for v in str_list:
+        try:
+            id_list.append(int(v))
+        except ValueError:
+            pass
+
+    if id_list:
+        return id_list
--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher
 
 from bot.handlers import start, message
 from bot.middlewares.auth import IsAllowedUserMiddleware
+from bot.utils import parse_allowed_users_ids
 from settings import Settings
 
 settings = Settings()
@@ -18,7 +19,7 @@ dp.include_router(message.router)
 
 dp.message.middleware(
     IsAllowedUserMiddleware(
-        allowed_users_ids=[int(v.strip()) for v in settings.allowed_users_ids.split(",")],
+        allowed_users_ids=parse_allowed_users_ids(settings.allowed_users_ids),
         silent=True
     )
 )
